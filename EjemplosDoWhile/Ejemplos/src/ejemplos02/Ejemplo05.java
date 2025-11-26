2f04dbf373f9d9cc42c13e312ac6d278d43143c6
jonathan <jonathanjavier2004@hotmail.com>
2025-11-26 12:23:57 -0500

    solucion problema 5

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo05.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo05.java
@@ -7,6 +7,10 @@
  sean ingresados por teclado por el usuario.
  */
 package ejemplos02;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -14,21 +18,16 @@ package ejemplos02;
 public class Ejemplo05 {
 
     public static void main(String[] args) {
-        // 2. numero, i[0, n]
-        int numero;
-        // 3. potencia, i[0, n]
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
         int potencia;
-        // 6. resultado, i[0, n]
-        int resultado;
-        // numero <-- 4
-        numero = 4;
-        // 8. potencia <-- 3
-        potencia = 3;
-        // 9. resultado <-- 1
-        resultado = 1;
-
+        int numero;
+        int resultado=1;
         int contador = 1;
-
+        System.out.println("el numero a potenciar");
+            numero = entrada.nextInt();
+            System.out.println("Ingrese la potencia que desea aplicar");
+            potencia = entrada.nextInt();
         do {
             resultado = resultado * numero;
             contador = contador + 1;
